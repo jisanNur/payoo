5059f09e65978f5ce03d0b29b0628e160a938263
jisanNur <jisanjisanur2002@gmail.com>
2025-05-27 20:12:14 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,9 @@
+// console.log('clicked')
+
+document.getElementById('login-btn')
+    .addEventListener("click", function (e) {
+
+        const phoneNumber = document.getElementById('phone-number').value;
+        console.log(phoneNumber)
+
+    })
