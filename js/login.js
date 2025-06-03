5fd6167bcdfe40d8a85033e2915f3ea31cdb9d19
jisanNur <jisanjisanur2002@gmail.com>
2025-06-03 18:51:02 +0600

    added add money input field

--- js/login.js
+++ js/login.js
@@ -1,9 +1,19 @@
 // console.log('clicked')
 
 document.getElementById('login-btn')
-    .addEventListener("click", function (e) {
+    .addEventListener("click", function () {
 
         const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber)
+        // console.log(phoneNumber)
+
+        const pinNumber = document.getElementById('pin-number').value;
+     
+      if(phoneNumber === '5' && pinNumber === '1111'){
+       window.location.href = '/payoo.html';
+      }
+      else{
+        alert('Please Enter Carrect number and pin')
+      }
+
 
     })
---
+++ payoo.js
@@ -0,0 +1,14 @@
+//  add money account
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(){
+
+
+        const addMoneyInput = document.getElementById('input-add-money').value
+        
+        const pinNumber = document.getElementById('input-pin-number').value
+
+        
+
+
+    })
